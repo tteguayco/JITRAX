f1b24b7c3ce1998d5c5eaa2a6d9da7a7aeef086b
Teguayco <teguayco95@gmail.com>
2017-02-15 12:30:10 +0000

    añadiendo panel de control para pestañas de álgebra relacional y sql

--- src/es/ull/etsii/jitrax/gui/main/CodeEditorPanel.java
+++ src/es/ull/etsii/jitrax/gui/main/CodeEditorPanel.java
@@ -2,12 +2,18 @@ package es.ull.etsii.jitrax.gui.main;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.border.LineBorder;
 
 import org.fife.ui.rsyntaxtextarea.AbstractTokenMakerFactory;
@@ -18,6 +24,9 @@ import org.fife.ui.rtextarea.RTextScrollPane;
 
 public class CodeEditorPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
+	
+	private static final String[] FONT_STYLES = { "Plain", "Arial", "Verdana"};
+	private static final String[] SGBD_LIST = { "PostgreSQL" };
 	
 	private static final String PANEL_TITLE = "Editor";
 	private static final String RA_SYNTAX_STYLE_ID = "text/RelationalAlgebra";
@@ -30,9 +39,20 @@ public class CodeEditorPanel extends JPanel {
 	
 	private static final int DEFAULT_FONT_SIZE = 16;
 	private static final int DEFAULT_FONT_STYLE = Font.PLAIN;
+	private static final int MIN_FONT_SIZE = 6;
+	private static final int MAX_FONT_SIZE = 18;
+	private static final int SPINNER_STEP = 1;
 	
 	private RSyntaxTextArea relationalAlgebraCodeEditor;
 	private RSyntaxTextArea sqlCodeEditor;
+	
+	private JComboBox raFontStyles;
+	private JComboBox sqlFontStyles;
+	private JComboBox sgbdList;
+	private JSpinner raFontSizeSelector;
+	private JSpinner sqlFontSizeSelector;
+	private SpinnerModel raFontSizeModel;
+	private SpinnerModel sqlFontSizeModel;
 	
 	private JTabbedPane tabbedPane = new JTabbedPane();
 	private JButton translateButton;
@@ -42,7 +62,7 @@ public class CodeEditorPanel extends JPanel {
 		relationalAlgebraCodeEditor = new RSyntaxTextArea();
 		sqlCodeEditor = new RSyntaxTextArea();
 		translateButton = new JButton("Translate to SQL");
-		executeButton = new JButton("Execute on Postgre");
+		executeButton = new JButton("Execute on");
 		
 		relationalAlgebraCodeEditor.setCaretColor(RA_CARET_COLOR);
 		String currentFontName = relationalAlgebraCodeEditor.getFont().getName();
@@ -59,19 +79,65 @@ public class CodeEditorPanel extends JPanel {
 		RTextScrollPane relationalAlgebraSP = new RTextScrollPane(relationalAlgebraCodeEditor);
 	    RTextScrollPane sqlSP = new RTextScrollPane(sqlCodeEditor);
 		
+	    // RA Code Editor parameters
+	    int raCodeEditorFontSize = relationalAlgebraCodeEditor.getFont().getSize();
+	    int raCodeEditorFontStyle = relationalAlgebraCodeEditor.getFont().getStyle();
+	    raFontStyles = new JComboBox(FONT_STYLES);
+		raFontSizeModel = new SpinnerNumberModel(raCodeEditorFontSize, 
+				MIN_FONT_SIZE, 
+				MAX_FONT_SIZE, 
+				SPINNER_STEP);
+		raFontSizeSelector = new JSpinner(raFontSizeModel);
+	    
+		// SQL Code Editor parameters
+		int sqlCodeEditorFontSize = sqlCodeEditor.getFont().getSize();
+		int sqlCodeEditorFontStyle = sqlCodeEditor.getFont().getSize();
+		sqlFontStyles = new JComboBox(FONT_STYLES);
+		sqlFontSizeModel = new SpinnerNumberModel(sqlCodeEditorFontSize, 
+				MIN_FONT_SIZE, 
+				MAX_FONT_SIZE, 
+				SPINNER_STEP); 
+		sqlFontSizeSelector = new JSpinner(sqlFontSizeModel);
+		sgbdList = new JComboBox(SGBD_LIST);
+		
+	    // Relational Algebra Tab
+	    JPanel raPanel = new JPanel(new BorderLayout());
+	    JPanel raControlPanel = new JPanel(new BorderLayout());
+	    JPanel translationPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	    JPanel raEditorElementsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+	    raPanel.add(relationalAlgebraSP, BorderLayout.CENTER);
+	    raPanel.add(raControlPanel, BorderLayout.SOUTH);
+	    translationPanel.add(translateButton);
+	    raControlPanel.add(translationPanel, BorderLayout.WEST);
+	    raControlPanel.add(raEditorElementsPanel);
+	    raEditorElementsPanel.add(new JLabel("Font: "));
+	    raEditorElementsPanel.add(raFontStyles);
+	    raEditorElementsPanel.add(new JLabel("Size: "));
+	    raEditorElementsPanel.add(raFontSizeSelector);
+	    
+	    // SQL Tab
+	    JPanel sqlPanel = new JPanel(new BorderLayout());
+	    JPanel sqlControlPanel = new JPanel(new BorderLayout());
+	    JPanel executionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	    JPanel sqlEditorElementsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+	    sqlPanel.add(sqlSP, BorderLayout.CENTER);
+	    sqlPanel.add(sqlControlPanel, BorderLayout.SOUTH);
+	    executionPanel.add(executeButton);
+	    executionPanel.add(sgbdList);
+	    sqlControlPanel.add(executionPanel, BorderLayout.WEST);
+	    sqlControlPanel.add(sqlEditorElementsPanel);
+	    sqlEditorElementsPanel.add(new JLabel("Font: "));
+	    sqlEditorElementsPanel.add(sqlFontStyles);
+	    sqlEditorElementsPanel.add(new JLabel("Size: "));
+	    sqlEditorElementsPanel.add(sqlFontSizeSelector);
+	    
 		setLayout(new BorderLayout());
 		tabbedPane = new JTabbedPane();
-		tabbedPane.add("Relational Algebra", relationalAlgebraSP);
-		tabbedPane.addTab("SQL", sqlSP);
-		tabbedPane.addTab("Parse Tree", null);
+		tabbedPane.add("Relational Algebra", raPanel);
+		tabbedPane.addTab("SQL", sqlPanel);
+		tabbedPane.addTab("Parse Tree", new JPanel());
 		tabbedPane.setFocusable(false);
 		add(tabbedPane, BorderLayout.CENTER);
-		
-		JPanel controlPanel = new JPanel();
-		controlPanel.add(translateButton);
-		controlPanel.add(executeButton);
-		
-		add(controlPanel, BorderLayout.SOUTH);
 		
 		LineBorder lineBorderPanel = (LineBorder) BorderFactory.createLineBorder(PANEL_BORDER_COLOR);
 		setBorder(BorderFactory.createTitledBorder(lineBorderPanel, PANEL_TITLE));
