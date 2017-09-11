393f96f18f98b39d6183196caee386e2a9e95977
tteguayco <teguayco95@gmail.com>
2017-09-11 16:46:09 +0100

    Adding C10N for internationalization

---
+++ src/es/ull/etsii/jitrax/translations/Translatable.java
@@ -0,0 +1,10 @@
+package es.ull.etsii.jitrax.translations;
+
+public interface Translatable {
+
+    /**
+     * Traduce los elementos de la clase que
+     * implementa esta interfaz.
+     */
+    public void translate();
+}
